1c664c35bbb3d186c207d1e0351922cb0ffdd52a
Brock Rowley <brockr@uoregon.edu>
2020-05-22 14:09:40 -0700

    ignore .DS_Store, working usethis and fs to create temporary-wd

---
+++ temporary-wd/Rowley_temp_script.R
@@ -0,0 +1,13 @@
+# write your function
+
+create_setup <- function(dir = getwd()) {
+  to_create <- c("data", "scripts", "plots")
+  purrr::walk(to_create, ~{
+    fs::dir_create(file.path(dir, .x))
+  })
+
+
+}
+fs::dir_delete("data")
+create_setup(here::here())
+create_setup(here::here("temporary-wd"))
