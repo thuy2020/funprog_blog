6f00f7ab693f5bf6f2517a009b3ec9c39c039a88
Brock Rowley <brockr@uoregon.edu>
2020-05-27 08:32:32 -0700

    Working on knitting blog formatting and language

--- Rowley_temp_script.R
+++ Rowley_temp_script.R
@@ -1,5 +1,3 @@
-# Write function
-
 create_setup <- function(dir = getwd()) {
   to_create <- c("data", "scripts", "plots")
   purrr::walk(to_create, ~{
@@ -8,6 +6,3 @@ create_setup <- function(dir = getwd()) {
 
 
 }
-# fs::dir_delete("data")
-create_setup(here::here())
-create_setup(here::here("temporary-wd"))
